d47b69028681d78311a319e7f73d206916c2e3f1
unknown <heitorcarrazzoni@gmail.com>
2023-05-28 19:10:48 -0300

    Teste

--- api/routes/apiRouter.js
+++ api/routes/apiRouter.js
@@ -9,10 +9,55 @@ const knex = require('knex')({
     client: 'pg',
     debug: true,
     connection: {
-        connectionString : process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl: { rejectUnauthorized: false },
     }
 })
+
+let checkToken = (req, res, next) => {
+    let authToken = req.headers["authorization"]
+    if (!authToken) {
+        res.status(401).json({ message: 'Token de acesso requerida' })
+    }
+    else {
+        let token = authToken.split(' ')[1]
+        req.token = token
+    }
+    jwt.verify(req.token, process.env.SECRET_KEY, (err, decodeToken) => {
+        if (err) {
+            res.status(401).json({ message: 'Acesso negado' })
+            return
+        }
+
+        req.usuarioId = decodeToken.id
+        next()
+    })
+}
+
+let isAdmin = (req, res, next) => {
+    knex
+        .select('*').from('usuario').where({ id: req.usuarioId })
+        .then((usuarios) => {
+            if (usuarios.length) {
+                let usuario = usuarios[0]
+                let roles = usuario.roles.split(';')
+                let adminRole = roles.find(i => i === 'ADMIN')
+                if (adminRole === 'ADMIN') {
+                    next()
+                    return
+                }
+                else {
+                    res.status(403).json({ message: 'Role de ADMIN requerida' })
+                    return
+                }
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Erro ao verificar roles de usuário - ' + err.message
+            })
+        })
+}
 
 apiRouter.post(endpoint + 'seguranca/login', (req, res) => {
     knex
@@ -71,7 +116,7 @@ apiRouter.post(endpoint + 'seguranca/register', (req, res) => {
 
 
 
-apiRouter.get(endpoint + 'produtos', function (req, res) {
+apiRouter.get(endpoint + 'produtos', checkToken, function (req, res) {
     knex.select('*').from('produto')
         .then(produtos => res.status(200).json(produtos))
         .catch(err => {
@@ -81,7 +126,7 @@ apiRouter.get(endpoint + 'produtos', function (req, res) {
         })
 })
 
-apiRouter.get(endpoint + 'produtos/:id', (req, res) => {
+apiRouter.get(endpoint + 'produtos/:id', checkToken, (req, res) => {
     knex.select('*').from('produto').where({ id: req.params.id })
         .then(produtos => res.status(200).json(produtos))
         .catch(err => {
@@ -91,7 +136,7 @@ apiRouter.get(endpoint + 'produtos/:id', (req, res) => {
         })
 })
 
-apiRouter.post(endpoint + 'produtos', (req, res) => {
+apiRouter.post(endpoint + 'produtos', checkToken, isAdmin, (req, res) => {
     knex('produto').insert({
         descricao: req.body.descricao,
         valor: req.body.valor,
@@ -105,7 +150,7 @@ apiRouter.post(endpoint + 'produtos', (req, res) => {
         })
 })
 
-apiRouter.put(endpoint + 'produtos/:id', (req, res) => {
+apiRouter.put(endpoint + 'produtos/:id', checkToken, isAdmin, (req, res) => {
     knex('produto').where('id', req.params.id).update({
         descricao: req.body.descricao,
         valor: req.body.valor,
@@ -118,7 +163,7 @@ apiRouter.put(endpoint + 'produtos/:id', (req, res) => {
         })
 })
 
-apiRouter.delete(endpoint + 'produtos/:id', (req, res) => {
+apiRouter.delete(endpoint + 'produtos/:id', checkToken, isAdmin, (req, res) => {
     knex('produto').where('id', req.params.id)
         .del().then(produtos => res.status(200).json({ "id": usuario.id }))
         .catch(err => {
